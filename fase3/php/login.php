b98942b200287389d2a44b0c465ae09ff1a8da4a
Andres <70452059+Andresmiusik@users.noreply.github.com>
2022-04-29 11:49:39 -0500

    Creación de Archivo Login

---
+++ fase3/php/login.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "12345678";
+$dbname = "bdunad31";
+
+// Creación de conexión con la base de datos
+$connn = mysli_connect($servername, $username, $password, $dbname);
+
+//Comprueba si la conexión ha funcionado en caso de que envie un valor 1
+if (!$conn){
+  die("Conexión fallida: " . mysqli_connect_error());
+}
+?>
